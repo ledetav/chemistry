ab81f9cf85c51604d1f49e6b7346834b80f83383
ledetav <nord.hilman@gmail.com>
2022-02-21 18:13:50 +0300

    Revert "Revert "other changes""
    This reverts commit 8e9298a99523a0d3d1421b8ceb99b3eeb73dd13d.

--- src/main.js
+++ src/main.js
@@ -6,5 +6,6 @@ import router from './router'
 import store from './store'
 
 import vSelect from "vue-select";
+import vModel from "@/views/testModel.vue";
 
-createApp(App).component("v-select", vSelect).use(store).use(router).mount('#app') 
+createApp(App).component("v-select", vSelect).component('v-model', vModel).use(store).use(router).mount('#app') 
